4fdb690b4624490ab41d6871b6c76cf9de6e0d8f
MinChul-Son <smc5236@naver.com>
2021-01-21 15:18:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '563011',
+    password: '000000',
     database: 'opentutorials'
 });
 db.connect();
